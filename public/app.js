d385edb3416740713ba9b7d7fe4c015c037a8955
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-09-26 11:42:26 -1000

    Reworking code

--- public/app.js
+++ public/app.js
@@ -1,46 +1,70 @@
 //jshint esversion: 6
 
 console.log("Sanity Check, REDDIT YO");
-
-let dankArray = [];
+let dankArray = []; // storage for Reddit info
 let wrapperDiv = document.getElementById("wrapper");
 
+//
+let query = {
+  url: "https://www.reddit.com/r/dankmemes.json",
+  method: "GET"
+};
 
-
-// images from reddit
-
-function retrieveAPI(url) {
+// Get API from URL
+function retrieveAPI(query, callBack) {
+  query = query || {}; // argument validation
+  query.method = query.method || "GET";
   let apiRequest = new XMLHttpRequest();
-
   apiRequest.addEventListener("load", requestListener());
-  apiRequest.open("GET", url);
+  apiRequest.open(query.method, query.url);
   apiRequest.send();
 }
 
 
+retrieveAPI(query, function(event){
+  console.log(event);
+  console.log(this);
+});
 
+// DATA PARSE + storing into
 function requestListener() {
   return function() {
     let parsedDocument = JSON.parse(this.responseText).data;
     dankArray = parsedDocument;
-    console.log(dankArray);
     makeDivs(dankArray);
   };
 }
 
+//subreddit is just name of subreddit
+function buildSubreddit(subreddit){
+  //variables
+  let query = { url: `http://www.reddit.com/r/${subreddit}.json`
+   };
+   let parent = document.getElementById("container");
+   let response = JSON.parse(this.responseText);
+   let posts = response.data.children;
+   //get subreddit data
+  request(query, function(){
+    //find or create parent dom
+
+  });
+}
 
 
 function makeDivs(array){
   for (let i = 0; i < 6; i++){
+    // creating DIVS for DOM attachment.
     let innerWrapper = document.createElement("DIV");
     let imagePreview = document.createElement("IMG");
     let titleDiv = document.createElement("DIV");
     let statsDiv = document.createElement("DIV");
 
+    // creating CLASS
     innerWrapper.className = "innerWrapper";
     titleDiv.className = "titleDiv";
     imagePreview.className = "imageDiv";
 
+    // appending REDDIT data to DOM
     imagePreview.src = array.children[i].data.url;
     titleDiv.innerHTML = array.children[i].data.title;
     statsDiv.innerHTML = "by " + array.children[i].data.author + " | " +
@@ -48,6 +72,7 @@ function makeDivs(array){
                       array.children[i].data.score + " likes, " +
                       array.children[i].data.num_comments + " comments";
 
+    // ORGANIZING Data in DIVS in DOM
     wrapperDiv.appendChild(innerWrapper);
     innerWrapper.appendChild(imagePreview);
     innerWrapper.appendChild(titleDiv);
@@ -56,7 +81,7 @@ function makeDivs(array){
 }
 
 
-retrieveAPI("https://www.reddit.com/r/dankmemes.json");
+// retrieveAPI("https://www.reddit.com/r/dankmemes.json");
 
 
 
